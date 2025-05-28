a446b5a560aecfcc17f732a07b87c8998ea97f80
YamotoYuki <hal.gotoutomoya@gmail.com>
2025-05-28 18:49:40 +0900

    2025/05/28 18:50

--- djangox/pages/urls.py
+++ djangox/pages/urls.py
@@ -5,4 +5,9 @@ from .views import HomePageView, AboutPageView
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
+    path("theater/", AboutPageView.as_view(), name="theater"),
+    path("ticket/", AboutPageView.as_view(), name="ticket"),
+    path("service/", AboutPageView.as_view(), name="service"),
+    path("access/", AboutPageView.as_view(), name="access"),
+    path("faq/", AboutPageView.as_view(), name="faq"),
 ]
